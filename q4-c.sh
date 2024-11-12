8c6ccfd32452b5723b7ec1f4f08d681439564cdf
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2024-11-12 16:17:11 +0530

    Question-4 (c) part completed

---
+++ q4-c.sh
@@ -0,0 +1,10 @@
+awk -F, '{if ($8 == 0) print $1, $4}' heart.csv > temp.txt
+gnuplot <<EOF
+set terminal pdf
+set output 'age_vs_cholesterol_correlation.pdf'
+set title "Correlation between Age and Cholesterol (No Heart Disease)"
+set xlabel "Age"
+set ylabel "Cholesterol"
+plot "temp.txt" using 2 with linespoints title "No Heart Disease"
+EOF
+rm temp.txt
